317d724e1410eadeffe9e6de43e3ed21fc5621a2
Mercedes Y Thompson <mythompson@bsu.edu>
2018-07-02 23:34:49 -0400

    Use map for clickbaits

--- src/ClickBait.js
+++ src/ClickBait.js
@@ -4,16 +4,28 @@ import Ad2 from './BaitAd2'
 import Ad3 from './BaitAd3'
 import Ad4 from './BaitAd4'
 
+
 class ClickBait extends Component {
+    constructor() {
+        super()
+        
+        this.state = {
+          Ads:[
+              Ad4,
+              Ad3,
+              Ad2,
+              Ad1,
+          ]
+        }
+      }
     render() {
         return(
 
 <div className="small-12 columns other-articles">
         <h2>From around the Realm</h2>
-        <Ad4 />
-        <Ad3 />
-        <Ad2 />
-        <Ad1 />
+        <ul>
+          { this.state.Ads.map(Ad => <Ad/>) }
+        </ul>
       </div>
       )
     }
